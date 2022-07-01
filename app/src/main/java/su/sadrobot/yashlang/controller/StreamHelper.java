ced427e542640fca3a1c504f59e8ec1e08df94f0
sadr0b0t <ii1i7@yandex.ru>
2022-07-01 23:59:26 +0300

    шапка с лицензией

--- app/src/main/java/su/sadrobot/yashlang/controller/StreamHelper.java
+++ app/src/main/java/su/sadrobot/yashlang/controller/StreamHelper.java
@@ -1,5 +1,25 @@
 package su.sadrobot.yashlang.controller;
 
+/*
+ * Created by Anton Moiseev (sadr0b0t) in 2022.
+ *
+ * Copyright (C) Anton Moiseev 2019 <github.com/sadr0b0t>
+ * ContentLoader.java is part of YaShlang.
+ *
+ * YaShlang is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YaShlang is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YaShlang.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import android.content.Context;
 
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
